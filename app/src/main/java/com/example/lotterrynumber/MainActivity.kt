90b87faf1b21353c1d27e8d156a34c07d62737a1
whtjtjddn <54929329+whtjtjddn@users.noreply.github.com>
2022-02-08 18:36:39 +0900

    Complete Coding Lottery System

--- app/src/main/java/com/example/lotterrynumber/MainActivity.kt
+++ app/src/main/java/com/example/lotterrynumber/MainActivity.kt
@@ -4,21 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.NumberPicker
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
-    private val clearButton: Button by lazy{
+    private val clearButton: Button by lazy {
         findViewById<Button>(R.id.clearButton)
     }
-    private val addButton: Button by lazy{
+    private val addButton: Button by lazy {
         findViewById<Button>(R.id.addButton)
     }
-    private val runButton: Button by lazy{
+    private val runButton: Button by lazy {
         findViewById<Button>(R.id.runButton)
     }
 
-    private val numberPicker:NumberPicker by lazy{
+    private val numberPicker: NumberPicker by lazy {
         findViewById<NumberPicker>(R.id.numberPicker)
     }
+
+    private val numberTextViewList: List<TextView> by lazy {
+        listOf<TextView>(
+            findViewById<TextView>(R.id.firstNumberTextView),
+            findViewById<TextView>(R.id.secondNumberTextView),
+            findViewById<TextView>(R.id.thirdNumberTextView),
+            findViewById<TextView>(R.id.fourthNumberTextView),
+            findViewById<TextView>(R.id.fifthNumberTextView),
+            findViewById<TextView>(R.id.sixthNumberTextView)
+        )
+    }
+
+    private var didRun: Boolean = false
+
+    private val pickNumberSet = mutableSetOf<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,20 +45,76 @@ class MainActivity : AppCompatActivity() {
         numberPicker.maxValue = 45
 
         initRunButton()
+        initAddButton()
+        initClearButton()
     }
-    private fun initRunButton(){
-        runButton.setOnClickListener{
+
+    private fun initRunButton() {
+        runButton.setOnClickListener {
+            if (pickNumberSet.size >= 6) {
+                Toast.makeText(this, "초기화 후에 다시 시도해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
             val list = getRandomNumber()
-        }
-    }
-    private fun getRandomNumber(): List<Int>{
-        val numberList = mutableListOf<Int>().apply{
-            for(i in 1..45){
-                this.add(i)
+
+            didRun = true
+
+            list.forEachIndexed { index, number ->
+                val textView = numberTextViewList[index]
+
+                textView.text = number.toString()
+                textView.isVisible = true
             }
         }
+    }
+
+    private fun initAddButton() {
+        addButton.setOnClickListener {
+            if (didRun) {
+                Toast.makeText(this, "초기화 후에 시도해 주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (pickNumberSet.size >= 6) {
+                Toast.makeText(this, "번호는 6개까지만 선택할 수 있습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (pickNumberSet.contains(numberPicker.value)) {
+                Toast.makeText(this, "이미 선택한 번호입니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val textView = numberTextViewList[pickNumberSet.size]
+            textView.isVisible = true
+            textView.text = numberPicker.value.toString()
+
+            pickNumberSet.add(numberPicker.value)
+        }
+    }
+
+    private fun initClearButton() {
+        clearButton.setOnClickListener {
+            pickNumberSet.clear()
+            numberTextViewList.forEach {
+                it.isVisible = false
+            }
+            didRun = false
+        }
+    }
+
+    private fun getRandomNumber(): List<Int> {
+        val numberList = mutableListOf<Int>()
+            .apply {
+                for (i in 1..45) {
+                    if (pickNumberSet.contains(i)) {
+                        continue
+                    }
+                    this.add(i)
+                }
+            }
         numberList.shuffle()
 
-        return numberList.subList(0,6)
+        val newList = pickNumberSet.toList() + numberList.subList(0, 6 - pickNumberSet.size)
+
+        return newList.sorted()
     }
 }
