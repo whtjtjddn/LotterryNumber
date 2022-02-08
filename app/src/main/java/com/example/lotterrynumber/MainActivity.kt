a918fcb3f25835632abd3fdd1c6f34f118026be7
whtjtjddn <54929329+whtjtjddn@users.noreply.github.com>
2022-02-08 18:52:30 +0900

    Submit setBackground by number

--- app/src/main/java/com/example/lotterrynumber/MainActivity.kt
+++ app/src/main/java/com/example/lotterrynumber/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
@@ -63,8 +64,12 @@ class MainActivity : AppCompatActivity() {
             list.forEachIndexed { index, number ->
                 val textView = numberTextViewList[index]
 
+                setNumberBackground(number, textView)
+
                 textView.text = number.toString()
                 textView.isVisible = true
+
+
             }
         }
     }
@@ -86,6 +91,7 @@ class MainActivity : AppCompatActivity() {
             val textView = numberTextViewList[pickNumberSet.size]
             textView.isVisible = true
             textView.text = numberPicker.value.toString()
+            setNumberBackground(numberPicker.value, textView)
 
             pickNumberSet.add(numberPicker.value)
         }
@@ -116,5 +122,19 @@ class MainActivity : AppCompatActivity() {
         val newList = pickNumberSet.toList() + numberList.subList(0, 6 - pickNumberSet.size)
 
         return newList.sorted()
+    }
+
+    private fun setNumberBackground(number: Int, textView: TextView) {
+        when (number) {
+            in 1..10 -> textView.background =
+                ContextCompat.getDrawable(this, R.drawable.circle_yellow)
+            in 11..20 -> textView.background =
+                ContextCompat.getDrawable(this, R.drawable.circle_blue)
+            in 21..30 -> textView.background =
+                ContextCompat.getDrawable(this, R.drawable.circle_red)
+            in 31..40 -> textView.background =
+                ContextCompat.getDrawable(this, R.drawable.circle_gray)
+            else -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_green)
+        }
     }
 }
